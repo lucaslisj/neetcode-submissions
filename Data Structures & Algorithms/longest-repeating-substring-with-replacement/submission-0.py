f087ec64c181b68a2eef9a8ec657aeb6e8fd9941
lucaslisj <lucaslisj25@gmail.com>
2026-05-19 14:04:24 -0500

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        p1 = 0
+        ans = 0
+        freq_table = {}
+        for p2 in range(len(s)):
+            #update the current letter into it
+            cur = s[p2]
+            if cur not in freq_table:
+                freq_table[cur] = 1
+            else:
+                freq_table[cur] += 1
+            
+            #check if the string from s[p1] to s[p2] (both inclusive) inclusive is valid
+
+            maxx = 0
+            for value in freq_table.values():
+                maxx = max(maxx,value)
+            
+            while (p2 - p1 + 1 - maxx > k): ## not valid
+                freq_table[s[p1]] -= 1
+                p1 += 1
+            
+            ans = max(ans, p2 - p1 + 1)
+        return ans
+            
+
+
+
+
+        
+            
+
